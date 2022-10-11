1807b78a0d0a3ca59de925b57c7aa6c8d89b11e4
Balazs Tieger <btieger@rapidminer.com>
2022-10-11 14:22:45 +0200

    read pems

--- main/src/main.py
+++ main/src/main.py
@@ -4,3 +4,12 @@ def read_pem(fname: str) -> str:
     with open(fname, 'r') as f:
         data = f.read()
     return RSA.import_key(data)
+
+def main():
+    pem_ca = read_pem('/pems/ca.pem')
+    pem_cert = read_pem('/pems/cert.pem')
+    pem_certKey = read_pem('/pems/certKey.pem')
+
+
+if __name__ == "__main__":
+    main()
