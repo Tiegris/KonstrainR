282ac71687962008ba3fe89a840860d3cf4838f5
Balazs Tieger <btieger@rapidminer.com>
2022-10-11 13:46:35 +0200

    error handling

--- main/src/cmd/main.go
+++ main/src/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
 )
@@ -30,7 +31,15 @@ func main() {
 		errorLogger.Fatalf("Failed to generate ca and certificate key pair: %v", err)
 	}
 
-	os.WriteFile("/pems/ca.pem", caPEM.Bytes(), 0644)
-	os.WriteFile("/pems/cert.pem", certPEM.Bytes(), 0644)
-	os.WriteFile("/pems/certKey.pem", certKeyPEM.Bytes(), 0644)	
+	savePem("/pems/ca.pem", caPEM)
+	savePem("/pems/cert.pem", certPEM)
+	savePem("/pems/certKey.pem", certKeyPEM)
+}
+
+
+func savePem(fname string, data *bytes.Buffer) {
+	err := os.WriteFile(fname, data.Bytes(), 0644)
+	if err != nil {
+		errorLogger.Fatalf("Failed to write file %s %v", fname, err)
+	}
 }
