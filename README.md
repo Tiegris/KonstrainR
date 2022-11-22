57e5f5729d3013e0e57db238fa5e5a67383cc860
Balazs Tieger <btieger@rapidminer.com>
2022-11-22 18:27:56 +0100

    m

--- README.md
+++ README.md
@@ -9,9 +9,15 @@ kubectl -n demo-ns delete pod alpine
 ```
 
 ```bash
+cd /pems && \
 openssl pkcs12 -export -in cert.pem -inkey key.pem -out keystore.p12 -name "sampleAlias" -password pass:foobar && \
 keytool -importkeystore -srckeystore keystore.p12 -srcstoretype pkcs12 -destkeystore keystore.jks -deststorepass foobar -srcstorepass foobar && \
-mkdir /agents/build/ && \
+mkdir /agents/build/ || true && \
 mv ./keystore.jks /agents/build/ && \
 cd /agents
+```
+
+```bash
+java -jar app.jar
+keytool -list -v -keystore build/keystore.jks -storepass foobar
 ```
--- main/KonstraineR-Agent/src/main/kotlin/me/btieger/Application.kt
+++ main/KonstraineR-Agent/src/main/kotlin/me/btieger/Application.kt
@@ -14,7 +14,8 @@ import java.security.cert.X509Certificate
 
 fun main() {
     val passwd = "foobar".toCharArray()
-    val keyStoreFile = "build/keystore.jks"
+    val keyStoreFilePath = "build/keystore.jks"
+    val keyStoreFile = File(keyStoreFilePath)
     val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
     FileInputStream(keyStoreFile).use { fis -> keyStore.load(fis, passwd) }
 
@@ -28,6 +29,7 @@ fun main() {
             keyAlias = "sampleAlias",
             keyStorePassword = { passwd },
             privateKeyPassword = { passwd }) {
+            keyStorePath = keyStoreFile
             port = 8443
             host = "0.0.0.0"
         }
