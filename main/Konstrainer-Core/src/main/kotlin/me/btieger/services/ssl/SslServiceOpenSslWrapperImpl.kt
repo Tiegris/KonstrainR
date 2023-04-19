00fd42f0f66a205397c0081ed2ac7d69e3e62f08
Balazs Tieger <btieger@rapidminer.com>
2023-04-19 16:13:03 +0200

    .

--- main/Konstrainer-Core/src/main/kotlin/me/btieger/services/ssl/SslServiceOpenSslWrapperImpl.kt
+++ main/Konstrainer-Core/src/main/kotlin/me/btieger/services/ssl/SslServiceOpenSslWrapperImpl.kt
@@ -7,7 +7,7 @@ import java.util.concurrent.ThreadLocalRandom
 import kotlin.streams.asSequence
 
 class SslServiceOpenSslWrapperImpl : SslService {
-    private val pwd = File("/kscore/ssl")
+    private val pwd = File("/app/ssl")
     private val rootCA: String
     init {
         if (pwd.exists() && !pwd.isDirectory())
@@ -19,12 +19,20 @@ class SslServiceOpenSslWrapperImpl : SslService {
 
         if (!File(pwd, "rootCA.key").exists())
             shell {
-                openssl("genrsa -out rootCA.key 4096")
+                openssl("genrsa", "-out", "rootCA.key", "4096")
             }
 
         if (!File(pwd, "rootCA.crt").exists())
             shell {
-                openssl("req -x509 -new -nodes -key rootCA.key -sha256 -days 365 -out rootCA.crt -subj \"/C=HU/O=me.btieger/CN=konstrainer-core\"")
+                openssl(
+                    "req", "-x509", "-new",
+                    "-nodes",
+                    "-key", "rootCA.key",
+                    "-sha256",
+                    "-days", "365",
+                    "-out", "rootCA.crt",
+                    "-subj", "\"/C=HU/O=me.btieger/CN=konstrainer-core\""
+                )
             }
 
         rootCA = getFile("rootCA.crt")
@@ -47,9 +55,21 @@ class SslServiceOpenSslWrapperImpl : SslService {
         val certName = "$folder/agent.crt"
         val csrName = "$folder/agent.csr"
         shell {
-            openssl("genrsa -out $keyName 2048")
-            openssl("req -new -sha256 -key $keyName -subj \"/C=HU/O=me.btieger/CN=$agentServiceName\" -out $csrName")
-            openssl("x509 -req -days 365 -in $csrName -out $certName -CAkey rootCA.key -CA rootCA.crt")
+            openssl("genrsa", "-out", keyName, "2048")
+            openssl(
+                "req", "-new", "-sha256",
+                "-key", keyName,
+                "-subj", "\"/C=HU/O=me.btieger/CN=$agentServiceName\"",
+                "-out", csrName
+            )
+            openssl(
+                "x509", "-req",
+                "-days", "365",
+                "-in", csrName,
+                "-out", certName,
+                "-CAkey", "rootCA.key",
+                "-CA", "rootCA.crt"
+            )
         }
         val key = getFile(keyName)
         val cert = getFile(certName)
@@ -57,7 +77,7 @@ class SslServiceOpenSslWrapperImpl : SslService {
     }
 
     private fun getFile(fileName: String) = File(pwd, fileName).readText(Charsets.US_ASCII)
-    private fun ShellScript.openssl(args: String) = command("openssl", listOf(args))
+    private fun ShellScript.openssl(vararg args: String) = command("openssl", args.toList())
     private fun shell(script: ShellScript.()->String) = shellRun(workingDirectory = pwd) {
         script()
     }
