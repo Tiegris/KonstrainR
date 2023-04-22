925f6a1f8fa04c103fda5336252978f7433e0f9e
Balazs Tieger <btieger@rapidminer.com>
2023-04-22 11:04:09 +0200

    .

--- main/KonstraineR-Agent/docker/startup.sh
+++ main/KonstraineR-Agent/docker/startup.sh
@@ -3,7 +3,7 @@ set -e -v -u -x
 
 app_root="/app"
 ssl_root="$app_root/tls-cert"
-openssl pkcs12 -export -in $ssl_root/cert.pem -inkey $ssl_root/key.pem -out $app_root/keystore.p12 -name "agent" -password pass:foobar
+openssl pkcs12 -export -in $ssl_root/cert.pem -inkey $ssl_root/key.pem -out $app_root/keystore.p12 -name "AgentCert" -password pass:foobar
 keytool -importkeystore -srckeystore $app_root/keystore.p12 -srcstoretype pkcs12 -destkeystore /app/keystore.jks -deststorepass foobar -srcstorepass foobar
 rm $app_root/keystore.p12
 
