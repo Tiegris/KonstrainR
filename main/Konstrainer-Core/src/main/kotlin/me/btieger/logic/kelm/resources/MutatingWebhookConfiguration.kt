ef933a01db58cbb41eabbe755e0505899eb18749
Balazs Tieger <btieger@rapidminer.com>
2023-04-22 21:26:25 +0200

    .

--- main/Konstrainer-Core/src/main/kotlin/me/btieger/logic/kelm/resources/MutatingWebhookConfiguration.kt
+++ main/Konstrainer-Core/src/main/kotlin/me/btieger/logic/kelm/resources/MutatingWebhookConfiguration.kt
@@ -3,6 +3,7 @@ package me.btieger.logic.kelm.resources
 import com.fkorotkov.kubernetes.admissionregistration.v1.*
 import me.btieger.dsl.*
 import io.fabric8.kubernetes.api.model.admissionregistration.v1.MutatingWebhookConfiguration
+import io.ktor.util.*
 import me.btieger.Config
 import me.btieger.logic.kelm.HelmService
 
@@ -16,7 +17,7 @@ fun HelmService.mutatingWebhookConfiguration(server: Server, cert: String, agent
                 admissionReviewVersions = listOf("v1", "v1beta1")
                 sideEffects = "None"
                 clientConfig {
-                    caBundle = cert
+                    caBundle = cert.encodeBase64()
                     service {
                         name = server.whName
                         namespace = config.namespace
