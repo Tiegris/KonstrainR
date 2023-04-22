5b94e4f24c582f92c2824dd4c4a264d9b4bcec6b
Balazs Tieger <btieger@rapidminer.com>
2023-04-22 23:32:50 +0200

    .

--- main/Konstrainer-Core/src/main/kotlin/me/btieger/controllers/ServerController.kt
+++ main/Konstrainer-Core/src/main/kotlin/me/btieger/controllers/ServerController.kt
@@ -35,7 +35,8 @@ fun Application.serverController() {
                 }
                 post("stop") {
                     exceptionGuard {
-                        TODO()
+                        serverService.stop(call.id)
+                        respond(HttpStatusCode.OK)
                     }
                 }
             }
