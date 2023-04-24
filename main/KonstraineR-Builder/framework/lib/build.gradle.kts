05a268f0c78a56ae6cac16d9f57a15fe8cc6a5fe
Balazs Tieger <btieger@rapidminer.com>
2023-04-24 23:11:23 +0200

    .

--- main/KonstraineR-Builder/framework/lib/build.gradle.kts
+++ main/KonstraineR-Builder/framework/lib/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    kotlin("jvm") version "1.8.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
 }
 
 repositories {
@@ -8,7 +8,7 @@ repositories {
     mavenCentral()
 }
 
-dependencies {    
-    implementation(files("/app/dsl.jar"))
+dependencies {
+    api(files("/app/dsl.jar"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 }
