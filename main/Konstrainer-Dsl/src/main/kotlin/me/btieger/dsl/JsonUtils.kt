0fcb3742280678abdf0f23d89f56dfeabc4766a5
Balazs Tieger <btieger@rapidminer.com>
2023-05-04 00:00:48 +0200

    .

--- main/Konstrainer-Dsl/src/main/kotlin/me/btieger/dsl/JsonUtils.kt
+++ main/Konstrainer-Dsl/src/main/kotlin/me/btieger/dsl/JsonUtils.kt
@@ -5,27 +5,38 @@ import kotlinx.serialization.json.*
 private val _regex = "/(?=(?:[^\']*\'[^\']*\')*[^\']*\$)".toRegex()
 
 @DslMarkerBlock
-infix fun JsonObject.jqx(s: String): JsonElement? {
-    val s = s.removePrefix("/").removeSuffix("/")
-    val splits = s.split(_regex)
-    var o : JsonElement? = this
-    for (getter in splits) {
-        val getterInt = getter.toIntOrNull()
-        o = if (getterInt == null) {
-            o?.jsonObject?.get(getter)
-        } else {
-            o?.jsonArray?.get(getterInt)
+infix fun JsonObject.jqx(s: String): JsonElement {
+    try {
+        val s = s.removePrefix("/").removeSuffix("/")
+        val splits = s.split(_regex)
+        var o : JsonElement = this
+        for (getter in splits) {
+            val getterInt = getter.toIntOrNull()
+            o = if (getterInt == null) {
+                o.jsonObject[getter] ?: JsonNull
+            } else {
+                o.jsonArray[getterInt]
+            }
+            if (o is JsonNull) return JsonNull
         }
-        o ?: return null
+        return o
+    } catch(e: Throwable) {
+        return JsonNull
     }
-    return o
+
 }
 
 @DslMarkerBlock object int
 @DslMarkerBlock object bool
 @DslMarkerBlock object double
 @DslMarkerBlock object string
-@DslMarkerBlock infix fun JsonElement?.parseAs(type :int): Int? = this?.jsonPrimitive?.content?.toIntOrNull()
-@DslMarkerBlock infix fun JsonElement?.parseAs(type :bool): Boolean? = this?.jsonPrimitive?.content?.toBooleanStrictOrNull()
-@DslMarkerBlock infix fun JsonElement?.parseAs(type: double): Double? = this?.jsonPrimitive?.content?.toDoubleOrNull()
-@DslMarkerBlock infix fun JsonElement?.parseAs(type :string): String? = this?.jsonPrimitive?.content
+fun <T> JsonElement.guard(fn: JsonElement.()->T) : T? {
+    return if (this is JsonPrimitive)
+        fn()
+    else
+        null
+}
+@DslMarkerBlock infix fun JsonElement.parseAs(type :int): Int? = guard { this.jsonPrimitive.content.toIntOrNull() }
+@DslMarkerBlock infix fun JsonElement.parseAs(type :bool): Boolean? = guard { this.jsonPrimitive.content.toBooleanStrictOrNull() }
+@DslMarkerBlock infix fun JsonElement.parseAs(type: double): Double? = guard { this.jsonPrimitive.content.toDoubleOrNull() }
+@DslMarkerBlock infix fun JsonElement.parseAs(type :string): String = guard { this.jsonPrimitive.content } ?: "null"
