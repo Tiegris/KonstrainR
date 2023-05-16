82cbe7d94b7362c122ca807c08bc150ae33501b8
Balazs Tieger <btieger@rapidminer.com>
2023-05-16 23:41:06 +0200

    .

---
+++ main/Konstrainer-Dsl/src/main/kotlin/me/btieger/builtins/EventLogger.kt
@@ -0,0 +1,28 @@
+package me.btieger.builtins
+
+import me.btieger.dsl.*
+
+val loggerDefaults = webhookConfigBundle {
+    namespaceSelector {
+        matchLabels {
+            "managed" eq "true"
+        }
+    }
+    failurePolicy(FAIL)
+    logRequest = true
+    logResponse = true
+    operations(CREATE, UPDATE)
+}
+
+val loggingServer = server("basic-webhook-rules") {
+    
+    webhook("log-deployments") {
+        apiGroups(APPS, CORE)
+        apiVersions(ANY)
+        resources(DEPLOYMENTS, PODS)
+        behavior {
+
+        }
+    }
+
+}
