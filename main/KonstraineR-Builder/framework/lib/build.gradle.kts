9bc94d8fc3badca856ef5be9826cdededbf7fea8
Balazs Tieger <btieger@rapidminer.com>
2023-05-27 20:06:35 +0200

    turbo builder

--- main/KonstraineR-Builder/framework/lib/build.gradle.kts
+++ main/KonstraineR-Builder/framework/lib/build.gradle.kts
@@ -5,11 +5,16 @@ plugins {
 
 repositories {
     mavenLocal()
-    mavenCentral()
+    //mavenCentral()
 }
 
 dependencies {
-    api(files("/app/libs/KonstrainerDsl-0.0.1-SNAPSHOT.jar"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("io.fabric8:kubernetes-client:6.6.0")
+    implementation(fileTree("/app/libs"))
+    //api(files("/app/libs/KonstrainerDsl-0.0.1-SNAPSHOT.jar"))
+    //implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    //implementation("io.fabric8:kubernetes-client:6.6.0")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
