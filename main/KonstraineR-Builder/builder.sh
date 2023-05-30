27aff4ab991b57758b5367491d3523c56e232e94
Balazs Tieger <btieger@rapidminer.com>
2023-05-30 17:40:38 +0200

    .

--- main/KonstraineR-Builder/builder.sh
+++ main/KonstraineR-Builder/builder.sh
@@ -18,12 +18,12 @@ log "BASE_URL = $base_url"
 log "DSL_ID = $dsl_id"
 
 function try_do() {
-    wget "${base_url}/${dsl_id}/file" -O "${file}" && log "Download done" || log "Download failed" && return 1
-    sed -i 's/package .*/package me.btieger/' "${file}" && log "Sed done" || log "Sed failed" && return 1
+    wget "${base_url}/${dsl_id}/file" -O "${file}" && log "Download done" || (log "Download failed" && error_report)
+    sed -i 's/package .*/package me.btieger/' "${file}" && log "Sed done" || (log "Sed failed" && error_report)
     cd /app/framework
-    ./gradlew jar >> $logsfile && log "Build done" || log "Build failed" && return 1
+    ./gradlew jar >> $logsfile && log "Build done" || (log "Build failed" && error_report)
     log "Compilation done"
-    curl -F filename="${jar}" -F upload=@${jar} -H "Authorization: ${secret}" -H "Content-Type: application/java-archive" "${base_url}/${dsl_id}/jar" || log "Upload failed" && return 1
+    curl -F filename="${jar}" -F upload=@${jar} -H "Authorization: ${secret}" -H "Content-Type: application/java-archive" "${base_url}/${dsl_id}/jar" || (log "Upload failed" && error_report)
 }
 
 function error_report() {
@@ -32,4 +32,4 @@ function error_report() {
     exit 1
 }
 
-try_do || error_report
+try_do
