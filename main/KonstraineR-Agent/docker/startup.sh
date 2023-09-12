e03e870005963d707c8cab6c869cb44e22314a54
Balazs Tieger <btieger@rapidminer.com>
2023-09-12 13:43:04 +0200

    .

--- main/KonstraineR-Agent/docker/startup.sh
+++ main/KonstraineR-Agent/docker/startup.sh
@@ -11,7 +11,7 @@ openssl pkcs12 -export -in $ssl_root/cert.pem -inkey $ssl_root/key.pem -out $app
 keytool -importkeystore -srckeystore $app_root/keystore.p12 -srcstoretype pkcs12 -destkeystore /app/keystore.jks -deststorepass foobar -srcstorepass foobar
 rm $app_root/keystore.p12
 
-wget "http://${KSR_CORE_BASE_URL}:8080/api/v1/dsls/${KSR_DSL_ID}/jar" -O "/app/libs/agentdsl.jar"
+wget "https://${KSR_CORE_BASE_URL}:8080/api/v1/dsls/${KSR_DSL_ID}/jar" -O "/app/libs/agentdsl.jar"
 echo ":/app/libs/agentdsl.jar" >> "/app/jib-classpath-file"
 
 exec "$@"
