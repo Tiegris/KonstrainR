be00f57261e50300160b5228ff6f0922a6dfbebb
Balazs Tieger <btieger@rapidminer.com>
2023-10-10 22:25:16 +0200

    .

--- main/DemoApp/apps/users/main.py
+++ main/DemoApp/apps/users/main.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 dbfile = os.getenv("DB_FILE", "users.db")
@@ -13,31 +14,40 @@ def get():
         return cur.execute("SELECT * FROM users").fetchall()
 
 
-@app.post("/users")
+@app.post("/login")
 def post():
-    data = request.get_json()
-    data = (data.name, data.fav_color, data.birt_year)
+    json = request.get_json()
+    data = (json["user"], json["passwd"])    
     with sqlite3.connect(dbfile) as con:
         cur = con.cursor()
-        cur.execute("INSERT INTO users VALUES(?, ?, ?)", data)
-        con.commit()
+        success = len(cur.execute(
+            "SELECT * FROM users WHERE name = ? AND passwd = ?", data
+        ).fetchall()) > 0
+        if success:
+            return "Logged in"
+        else:
+            logging.warning(f"Invalid login atempt: {json}")
+            return "Invalid pass"
+            
 
 
 def init_db():
     with sqlite3.connect(dbfile) as con:
         cur = con.cursor()
-        cur.execute("CREATE TABLE users(name, fav_color, birt_year)")
-        cur.execute("""
+        cur.execute("CREATE TABLE users(name, passwd, fav_color, birt_year)")
+        cur.execute(
+            """
         INSERT INTO users VALUES
-            ('John Athan', 'red', 1864),
-            ('Ida Red', 'red', 1935),
-            ('Granny Smith', 'green', 1868),
-            ('John A. Gold', 'red', 1943)
-        """)
+            ('John Athan', 'password', 'red', 1864),
+            ('Ida Red', '1234', 'red', 1935),
+            ('Granny Smith', 'ledig', 'green', 1868),
+            ('John A. Gold', 'apples', 'red', 1943)
+        """
+        )
         con.commit()
 
 
 if not os.path.isfile(dbfile):
-   init_db()
+    init_db()
 
 app.run(host="0.0.0.0", port=8078)
